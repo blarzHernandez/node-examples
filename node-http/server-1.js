20dee67c14f10d9cdda96ebae1f7a8f01e1e6bc8
Roberto Hernandez <roberto.hernandez@gruporaf.com>
2017-02-14 16:14:37 -0600

    Add server-1.js file

--- node-http/server-1.js
+++ node-http/server-1.js
@@ -3,11 +3,15 @@ var http = require('http');
 
 //Hostaname
 var hostname  = 'localhost';
-var port      =  3000;
+var port      =  3001;
 
 //Create server
 var server = http.createServer(function(req,res){
   console.log(req.headers);
   res.writeHead(200, {'Content-Type':'text/html'});
   res.end('<h1>Hello World!</h1>');
+});
+
+server.listen(port,hostname,function(){
+console.log(`Server running at http://${hostname}:${port}/`);
 });
