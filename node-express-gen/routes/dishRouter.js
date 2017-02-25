03cfd7df200e39f885611eb44e7d734df259c6ec
Blarz hdz <info.redpaz@gmail.com>
2017-02-25 16:41:00 -0600

    dishrouter, promorouter and leaderrouter added

---
+++ node-express-gen/routes/dishRouter.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var dRouter = express.Router();
+
+//For collections
+dRouter.route('/')
+.all(function(req,res,next){
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  next();
+
+})
+.get(function(req,res,next){
+  res.end("Will send all the dishes for you");
+})
+.post(function(req,res,next){
+
+  res.end("Will add the dish: " + req.body.name + ' with details: ' + req.body.description);
+})
+.delete(function(req,res,next){
+  res.end("Deleting all dishes ok");
+});
+
+//For item
+dRouter.route('/:dishId')
+  .all(function(req,res,next){
+      res.writeHead(200, {'Content-Type':'text/plain'});
+      next();
+  })
+  .get(function(req,res,next){
+      res.end('Will send details about the dish: ' + req.params.dishId + ' for you');
+  })
+  .put(function(req,res,next){
+      res.write('We are updating the dish: ' + req.params.dishId + '\n');
+      res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+  }).
+  delete(function(req,res,next){
+      res.end("Deleting dish: " + req.params.dishId);
+  });
+
+//export module
+module.exports = dRouter;
